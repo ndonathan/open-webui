6a445293962cfb5f628320d391cce081302eb3bd
Nick Donathan <31076657+ndonathan@users.noreply.github.com>
2024-03-04 22:57:47 -0600

    Update vite.config.ts
    adding certificate config. May need to add proxy. Will also need to update build steps to include https option.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	server: {
+        https: {
+            key: fs.readFileSync(`${__dirname}/cert/key.pem`),
+            cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
+		}
+    	}
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version)
 	}

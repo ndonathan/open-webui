f94cde8370757ed46c440a109feedcbb1f5b1b9e
Nick Donathan <31076657+ndonathan@users.noreply.github.com>
2024-03-06 17:52:27 -0600

    Update vite.config.ts
    updated formatting and TLS cert path

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
-        https: {
-            key: fs.readFileSync(`${__dirname}/cert/key.pem`),
-            cert: fs.readFileSync(`${__dirname}/cert/cert.pem`)
+		https: {
+            		key: fs.readFileSync(`/etc/ssl/certs/private.key`),
+            		cert: fs.readFileSync(`/etc/ssl/certs/public.crt`),
 		}
-    	}
+    	},
 	define: {
 		APP_VERSION: JSON.stringify(process.env.npm_package_version)
 	}
